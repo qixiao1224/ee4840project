cab3f12ddf0acbaf43714bd5d74ebfa089552b9f
qixiao1224 <zqx981224@gmail.com>
2023-05-05 16:51:52 -0400

    update testbench

--- project-hw/npu_tb/memory_test.v
+++ project-hw/npu_tb/memory_test.v
@@ -3,7 +3,8 @@
 
 `define IMG_INPUT "../img0_z.txt"
 `define CONV_FILTER_INPUT "../weight_bias_conv2d1.txt"
-`define CONV1_RESULT "../conv2d1_result.txt"
+
+`define POOLING1_RESULT "../pooling1_result.txt"
 
 module testbench(  );
 
@@ -18,6 +19,12 @@ module testbench(  );
   integer conv_filter_input;
   integer conv1_result;
   integer i;
+  integer pooling1_result;
+  
+  reg [10:0] error_count_layer12;
+
+  reg [7:0] out0, out1, out2, out3;
+  reg [7:0] out0_ff, out1_ff, out2_ff, out3_ff;
 
   reg [7:0] conv_filter_array;
   reg [7:0] conv1_result_array;
@@ -41,8 +48,8 @@ initial begin
 
 img_input = $fopen (`IMG_INPUT, "r");
 conv_filter_input = $fopen (`CONV_FILTER_INPUT, "r");
-conv1_result = $fopen (`CONV1_RESULT, "r");
 
+pooling1_result = $fopen(`POOLING1_RESULT,"r");
 
 if (!img_input) begin 
     $display("Cannot Open IMG!");
@@ -54,7 +61,8 @@ if (!conv_filter_input) begin
     $finish;
 end
 
-if (!conv1_result) begin 
+
+if (!pooling1_result) begin 
     $display("Cannot Open IMG!");
     $finish;
 end
@@ -62,6 +70,7 @@ end
 clk = 0;
 reset = 0;
 writedata = 0;
+error_count = 0;
 
 @(posedge clk); 
 reset = 1;
@@ -101,7 +110,12 @@ for (i = 0; i < 18815; i = i + 1)begin
     @(posedge clk);
 end
 
+for (i = 0; i < 18815; i = i + 1)begin
+    $fscanf(conv_filter_input, "%8b", tmp4);
+    writedata = tmp4;
 
+    @(posedge clk);
+end
 
 
  @(posedge clk);
@@ -115,9 +129,64 @@ control_reg = 32'h2;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*Layer 12 TESTING*/
+for (i = 0; i < 1568; i = i + 1) begin
+	out0 = testbench.mem_top1.memory_read1.res_ram0.mem[i];
+        out1 = testbench.mem_top1.memory_read1.res_ram1.mem[i];
+	out2 = testbench.mem_top1.memory_read1.res_ram2.mem[i];
+	out3 = testbench.mem_top1.memory_read1.res_ram3.mem[i];
+	
+	$fscanf(pooling1_result,"%b",out0_ff);
+	$fscanf(pooling1_result,"%b",out1_ff);
+	$fscanf(pooling1_result,"%b",out2_ff);
+	$fscanf(pooling1_result,"%b",out3_ff);
+
+	if (out0 != out0_ff) begin 
+              error_count_layer12 = error_count + 1;
+        end
+	if (out1 != out1_ff) begin 
+              error_count_layer12 = error_count + 1;
+        end
+	if (out2 != out2_ff) begin 
+              error_count_layer12 = error_count + 1;
+        end
+	if (out3 != out3_ff) begin 
+              error_count_layer12 = error_count + 1;
+        end
+
+end
+
+/*Layer 34 TESTING*/
+for (i = 1568; i < 1568+289; i = i + 1) begin
+	out0 = testbench.mem_top1.memory_read1.res_ram0.mem[i];
+        out1 = testbench.mem_top1.memory_read1.res_ram1.mem[i];
+	out2 = testbench.mem_top1.memory_read1.res_ram2.mem[i];
+	out3 = testbench.mem_top1.memory_read1.res_ram3.mem[i];
+	
+	$fscanf(pooling2_result,"%b",out0_ff);
+	$fscanf(pooling2_result,"%b",out1_ff);
+	$fscanf(pooling2_result,"%b",out2_ff);
+	$fscanf(pooling2_result,"%b",out3_ff);
+
+	if (out0 != out0_ff) begin 
+              error_count_layer34 = error_count + 1;
+        end
+	if (out1 != out1_ff) begin 
+              error_count_layer34 = error_count + 1;
+        end
+	if (out2 != out2_ff) begin 
+              error_count_layer34 = error_count + 1;
+        end
+	if (out3 != out3_ff) begin 
+              error_count_layer34 = error_count + 1;
+        end
+
+end
 
 
-
+/* ERROR DISPLAY*/
+$display ("Error of Layer 12 = %d" ,error_count_layer12);
+$display ("Error of Layer 34 = %d" ,error_count_layer34);
 
   $stop;
 
