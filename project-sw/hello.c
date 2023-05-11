b0dc24bb2dbd0cde816df5429bd8ef656ff39065
hz2833 <hz2833@micro35.ee.columbia.edu>
2023-05-11 05:07:05 -0400

    add back usleep

--- project-sw/hello.c
+++ project-sw/hello.c
@@ -297,12 +297,12 @@ int main()
   printf("send control\n");
   int i =1;
   set_control(&i);  
-  usleep(0.01);
+//  usleep(0.01);
   int k =0; 
   for(k=0;k<count;k++)
   {
      set_data(&d[k]);
-     usleep(0.01);
+//     usleep(0.01);
 //     printf("set_time = %d\n",(int)(end_set-start_set));
 //     s_cycle= s_cycle + (long)(end_set - start_set);
   }
@@ -312,13 +312,13 @@ int main()
   i=2;
   clock_t start=clock();
   set_control(&i);
-  usleep(0.01);
+//  usleep(0.01);
   int ready = 0;
   int counter = 0;
   while(ready == 0) {
 	read_ready(&ready);
 	counter++;
-        usleep(0.01);
+//        usleep(0.01);
 } 
  
 printf("ready: %d \n", ready);
